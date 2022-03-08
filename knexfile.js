55960482a31e548a61f8abf621e7600089d3ffb2
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2022-03-08 14:12:19 +0100

    Migrations now work

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,42 @@
 // Update with your config settings.
-const sharedConfig = {
-  client: "sqlite3",
-  migrations: { directory: "./data/migrations" },
-  seeds: { directory: "./data/seeds" },
-  //sqlite3
-  useNullAsDefault: true,
-  pool: {
-    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
-  },
-};
 
 module.exports = {
   development: {
-    ...sharedConfig,
-    connection: { filename: "./data/user.db3" },
+    client: "sqlite3",
+    connection: {
+      filename: "./dev.sqlite3",
+    },
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: "./data/user.test.db3" },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
   },
-  production: {},
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
