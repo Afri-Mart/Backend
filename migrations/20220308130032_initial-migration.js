bb7a9a0e6ec423c9d7b836672b49971de4a1d23e
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2022-03-08 14:12:48 +0100

    Migrations now work

--- migrations/20220308130032_initial-migration.js
+++ migrations/20220308130032_initial-migration.js
@@ -1,15 +1,30 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+exports.up = async function (knex) {
+  return knex.schema
+    .createTable("roles", (tbl) => {
+      tbl.increments();
+      tbl.string("name", 128).notNullable();
+    })
+    .createTable("users", (tbl) => {
+      tbl.increments("users_id");
+      tbl.string("username", 128).notNullable().unique();
+      tbl.string("password", 256).notNullable();
+      tbl.integer("role").unsigned().defaultTo(1);
+      tbl.foreign("role").references("roles.id");
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
+exports.down = async function (knex) {
+  return knex.schema
+    .dropTableIfExists("users")
+    .dropTableIfExists("roles")
+    .dropTableIfExists("class_clients")
+    .dropTableIfExists("classes");
 };
