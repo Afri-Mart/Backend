a0223055b4d1978567cb214d1f64d41b1b996b35
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2022-03-14 09:44:11 +0100

    Commented out cors

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const server = require("./server.js");
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
