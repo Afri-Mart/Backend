6806b66d22c7d38c063c671173fbac8bb608013d
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2022-03-15 18:54:58 +0100

    changed item_id

--- migrations/20211115082418_items.js
+++ migrations/20211115082418_items.js
@@ -1,18 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable("items", (items) => {
     items.increments();
+    items.increment("items_id", { primaryKey: false });
     items.string("Title", 255).unique().notNullable();
     items.string("Price", 10).notNullable();
     items.integer("Qty", 10).notNullable();
     items.string("image", 256);
-    items
-      .integer("items_id")
-      .unsigned()
-      .notNullable()
-      .references("items_id")
-      .inTable("items")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
   });
 };
 
