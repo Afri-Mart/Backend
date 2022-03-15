3b4dd5d37f8c78c9313493c7e8d4a99b9d3c243a
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2022-03-15 18:41:24 +0100

    Connection refused to work

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
     client: "pg",
     connection: {
       connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized },
+      ssl: { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
